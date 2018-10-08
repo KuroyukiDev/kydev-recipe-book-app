8373a9b70b377e9ac39b53d6f1088b92879cd66b
KuroyukiDev <kuroyuki.dev@gmail.com>
2018-10-07 17:52:16 -0700

    Update Navigation and display selected recipe details on click events

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,15 +7,18 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Green Tea Mochi', 'Yummy Mochi flavored with Matcha Poweder', 'http://finedininglovers.cdn.crosscast-system.com/BlogPost/l_13010_green-tea-mochi.jpg'),
-    new Recipe('Green Tea Mochi', 'Yummy Mochi flavored with Matcha Poweder', 'http://finedininglovers.cdn.crosscast-system.com/BlogPost/l_13010_green-tea-mochi.jpg'),
-    new Recipe('Green Tea Mochi', 'Yummy Mochi flavored with Matcha Poweder', 'http://finedininglovers.cdn.crosscast-system.com/BlogPost/l_13010_green-tea-mochi.jpg')
+    new Recipe('Green Tea Mochi', 'Yummy Mochi flavored with Matcha Powder!', 'http://finedininglovers.cdn.crosscast-system.com/BlogPost/l_13010_green-tea-mochi.jpg'),
+    new Recipe('Yakisoba with Chicken', 'Japanese buckwheat flour noodles with chicken at their best!', 'https://images.media-allrecipes.com/userphotos/560x315/2220105.jpg'),
+    new Recipe('Beef Kushiyaki', 'A Japanese favorite!', 'https://images.media-allrecipes.com/userphotos/560x315/879272.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
